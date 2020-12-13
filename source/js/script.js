fd90b6abba03e03b697f744e1a456cec1d546340
Natalia Khrustaleva <hrustalyova@mail.ru>
2020-12-13 18:45:37 +0300

    pixel pefect закрытого меню на мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,10 @@
-var navMain = document.querySelector('.main-nav');
+var navMain = document.querySelector('.main-nav__list');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav__list--closed');
+navToggle.classList.add('main-nav__toggle--closed');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+  navMain.classList.toggle('main-nav__list--closed');
+  navToggle.classList.toggle('main-nav__toggle--closed');
 });
