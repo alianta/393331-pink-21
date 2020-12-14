561eb0f385dfb5b6407d3ed018669301fd91c447
Natalia Khrustaleva <hrustalyova@mail.ru>
2020-12-14 22:35:08 +0300

    pixel perfect открытого меню на мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,13 @@
 var navMain = document.querySelector('.main-nav__list');
+var heder = document.querySelector('.header');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.add('main-nav__list--closed');
+heder.classList.add('header--nav-closed');
 navToggle.classList.add('main-nav__toggle--closed');
 
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('main-nav__list--closed');
+  heder.classList.toggle('header--nav-closed');
   navToggle.classList.toggle('main-nav__toggle--closed');
 });
