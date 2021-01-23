8151ed8e9832774dad7cbdf8f98e836eb284c3f1
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-01-23 19:06:49 +0300

    добавлено отслеживание измнений js файла

--- gulpfile.js
+++ gulpfile.js
@@ -133,6 +133,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/js/*.js", gulp.series("scripts"));
   gulp.watch("source/*.html").on('change', gulp.series("html", sync.reload));
 }
 
